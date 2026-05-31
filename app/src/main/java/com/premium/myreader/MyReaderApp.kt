e02b38cd5c9ee630db76ef604920ef98b868d235
Saif-Adil-Bro <ammamun94@gmail.com>
2026-05-31 11:56:19 +0600

    Added MyReaderApp and initialized Firebase

--- app/src/main/java/com/premium/myreader/MyReaderApp.kt
+++ app/src/main/java/com/premium/myreader/MyReaderApp.kt
@@ -1,6 +1,14 @@
 package com.premium.myreader
+
 import android.app.Application
+import com.google.firebase.FirebaseApp
 import dagger.hilt.android.HiltAndroidApp
 
 @HiltAndroidApp
-class MyReaderApp : Application()
+class MyReaderApp : Application() {
+    override fun onCreate() {
+        super.onCreate()
+        // ফায়ারবেস চালু করার মূল কোড
+        FirebaseApp.initializeApp(this)
+    }
+}
