cba9ac9798e11b0b21cf89eabf0f3a8c2e49e379
Saif-Adil-Bro <ammamun94@gmail.com>
2026-06-03 14:31:08 +0600

    Hard reset and clean syntax for MyReaderApp.kt via termux

--- app/src/main/java/com/premium/myreader/MyReaderApp.kt
+++ app/src/main/java/com/premium/myreader/MyReaderApp.kt
@@ -11,20 +11,18 @@ import io.github.jan-tennert.supabase.storage.Storage
 class MyReaderApp : Application() {
 
     companion object {
-        // সুপাবেস ক্লায়েন্ট প্লাগইন যা পুরো অ্যাপে এক্সেস করা যাবে
         lateinit var supabase: SupabaseClient
     }
 
     override fun onCreate() {
         super.onCreate()
 
-        // সুপাবেস প্লাগইন ইনিশিয়ালের মূল কোড
         supabase = createSupabaseClient(
             supabaseUrl = "https://phkjmdmjmoxpdduoyfxp.supabase.co",
             supabaseKey = "sb_publishable_tMNtA0sQs4o3m8iXr2meqg_dIRjCaIN"
         ) {
-            install(Postgrest) // ডাটাবেস প্লাগইন
-            install(Storage)   // ফাইল স্টোরেজ প্লাগইন
+            install(Postgrest)
+            install(Storage)
         }
     }
 }
